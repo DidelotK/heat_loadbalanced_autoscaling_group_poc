6b1ccb529b6d53a9eed33fa5e5423ac4263e0444
DidelotK <didelotkev@gmail.com>
2018-04-16 16:54:10 +0200

    Installation des roles dans user data avant lancement du playbook de configuration

--- scripts/launch_ansible.sh
+++ scripts/launch_ansible.sh
@@ -3,7 +3,10 @@ echo "Clone git project"
 cd /opt
 git clone https://github.com/DidelotK/heat_loadbalanced_autoscaling_group_poc
 
-echo "Launch ansible"
+echo "Install ansible roles"
 cd /opt/heat_loadbalanced_autoscaling_group_poc/ansible
+ansible-galaxy install -p ./roles -r requirements.yml
+
+echo "Launch ansible"
 ansible-playbook main.yml
 
